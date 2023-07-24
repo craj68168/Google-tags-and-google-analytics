0a5809d5d50184ff5b4079d97fd55b92d88ba60e
Raj Chaudhary <craj68168@gmail.com>
2023-07-24 22:39:06 +0545

    feat:fixes some changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,30 @@
+"use client"
+import styled from "styled-components";
+
+const Div = styled.div`
+  max-width: 760px;
+  margin: 40px auto;
+  letter-spacing: 1px;
+  line-height: 1.6em;
+`;
 export default function Home() {
   return (
-    <div>Hello</div>
-  )
+    <Div>
+      <h2>Welcome to Gaming Vibes</h2>
+      <div>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
+          deleniti rem aspernatur odit hic autem neque repellat alias? Debitis
+          veniam inventore ipsum similique quos animi ipsa asperiores fuga dolor
+          id. Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
+          deleniti rem aspernatur odit hic autem neque repellat alias? Debitis
+          veniam inventore ipsum similique quos animi ipsa asperiores fuga dolor
+          id. Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
+          deleniti rem aspernatur odit hic autem neque repellat alias? Debitis
+          veniam inventore ipsum similique quos animi ipsa asperiores fuga dolor
+          id.
+        </p>
+      </div>
+    </Div>
+  );
 }
